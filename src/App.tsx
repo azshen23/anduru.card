f7456af6bf5dca4e1aba4008b017652e3d81fcaf
Andrew Shen <azshen23@gmail.com>
2025-03-05 22:00:25 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Card />
+      <Analytics />
     </>
   );
 }
