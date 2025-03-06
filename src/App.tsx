96b428fc2d1fcc00c12d03addc4f8f0274c6ed76
Andrew Shen <azshen23@gmail.com>
2025-03-05 22:31:32 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Analytics } from "@vercel/analytics/react";
 import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Card />
-      <Analytics />
     </>
   );
 }
